4903bfd96c3b654dc9265268515092649eb6d614
Fred Young <nyang@netvigator.com>
2022-08-01 20:06:57 +0800

    add

---
+++ cnd/hello.py
@@ -0,0 +1,13 @@
+"""hello.py
+"""
+#! /usr/bin/evn python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return ("Hello world!")
+
+if __name__ == '__main__':
+    app.run(port=8888, debug=True)
